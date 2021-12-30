c6dbfeadd957679fbda2e3c8c680b894f853184b
dogustuluk <dogus.tuluk@gmail.com>
2021-12-30 23:52:49 +0300

    Add project files.

---
+++ BarcodeScanner2/BarcodeScanner2/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace BarcodeScanner2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushAsync(scan);
+            scan.OnScanResult += (result) =>
+              {
+                  Device.BeginInvokeOnMainThread(async () =>
+                  {
+                     await Navigation.PopAsync();
+                      mycode.Text = result.Text;
+                  });
+              };
+        }
+
+
+    }
+}
